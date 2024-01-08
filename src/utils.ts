6f8c30b5c2ba7f1a2e5b2451a09663830343d6d7
User <user@192.168.0.21>
2024-01-08 23:23:19 +0300

    rels #i4. commitmessage

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,26 @@
-export const getTestResult = (values: number[]): string => {
-  const result = "";
+import { Result } from "antd";
 
-  return result;
+export const getTestResult = (values: number[]): string => {
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === 1) {
+      count1++;
+    } else if (values[i] === 2) {
+      count2++;
+    } else {
+      count3++;
+    }
+  }
+
+  let maxCount = Math.max(count1, count2, count3);
+  if (maxCount == count1) {
+    return `Кабачок`;
+  } else if (maxCount == count2) {
+    return `Баклажан`;
+  } else {
+    return `Тыковка`;
+  }
 };
