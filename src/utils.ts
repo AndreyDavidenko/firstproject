2c2eeaa4696d3fa7b68bf47732f3cc7576fa4d56
User <user@192.168.0.21>
2024-01-09 11:17:49 +0300

    rels #i4. replaced for with .forEach, returned the name to the variables

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,24 @@
 export const getTestResult = (values: number[]): string => {
-  let namRespons1 = 0;
-  let namRespons2 = 0;
-  let namRespons3 = 0;
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
 
   values.forEach((elem) => {
     if (elem === 1) {
-      namRespons1++;
+      count1++;
     } else if (elem === 2) {
-      namRespons2++;
+      count2++;
     } else if (elem === 3) {
-      namRespons3++;
+      count3++;
     }
   });
 
-  let maxNamRespons = Math.max(namRespons1, namRespons2, namRespons3);
-  if (maxNamRespons == namRespons1) {
+  let maxCount = Math.max(count1, count2, count3);
+  if (maxCount == count1) {
     return `Кабачок`;
-  } else if (maxNamRespons == namRespons2) {
+  } else if (maxCount == count2) {
     return `Баклажан`;
-  } else if (maxNamRespons == namRespons3) {
+  } else if (maxCount == count3) {
     return `Тыковка`;
   }
 };
