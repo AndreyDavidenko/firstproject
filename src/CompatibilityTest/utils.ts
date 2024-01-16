77534ee51d3abbe4e384a9b7822589ad33a2920e
User <user@MacBook-Pro-User.local>
2024-01-16 20:29:23 +0300

    rels #i6. calculation of compatibility percentage

--- src/CompatibilityTest/utils.ts
+++ src/CompatibilityTest/utils.ts
@@ -1,3 +1,13 @@
 export const getTestResult = (boyAnswers: number[], girlAnswers: number[]): string => {
-  return "kuku";
+  let count1 = 0;
+
+  boyAnswers.forEach((boyAnswer, index) => {
+    if (boyAnswer === girlAnswers[index]) {
+      count1++;
+    }
+  });
+
+  let percent = (count1 / boyAnswers.length) * 100;
+  console.log(count1);
+  return "Вы подходите друг другу на ${percent}%";
 };
